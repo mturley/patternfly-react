fd3dc6f1bb04b01da2bb8c7a332e9d25a8b22957
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-09-26 16:23:36 -0400

    Revert "fix issue in Internal Dropdown Item breaking my example"
    This reverts commit 820d09b3318f713e4518d8ae9d89718cf54d599c.

--- packages/patternfly-4/react-core/src/components/Dropdown/InternalDropdownItem.tsx
+++ packages/patternfly-4/react-core/src/components/Dropdown/InternalDropdownItem.tsx
@@ -30,8 +30,6 @@ export interface InternalDropdownItemProps extends React.HTMLProps<HTMLAnchorEle
   };
   /** Callback for click event */
   onClick?: (event: React.MouseEvent<any> | React.KeyboardEvent | MouseEvent) => void;
-  /** TODO */
-  id?: string;
 }
 
 export class InternalDropdownItem extends React.Component<InternalDropdownItemProps> {
@@ -90,7 +88,6 @@ export class InternalDropdownItem extends React.Component<InternalDropdownItemPr
       href,
       tooltip,
       tooltipProps,
-      id,
       ...additionalProps
     } = this.props;
     const Component = component as any;
@@ -136,7 +133,6 @@ export class InternalDropdownItem extends React.Component<InternalDropdownItemPr
                   onSelect(event);
                 }
               }}
-              id={id}
             >
               {renderWithTooltip(
                 isComponentReactElement ? React.cloneElement(Component as React.ReactHTMLElement<any>, {
