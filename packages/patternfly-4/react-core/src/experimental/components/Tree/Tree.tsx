a3ad85bcda82a4bd2a82fa63f47371fabce4509d
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-11-12 13:13:58 -0500

    Add basic table structure

--- packages/patternfly-4/react-core/src/experimental/components/Tree/Tree.tsx
+++ packages/patternfly-4/react-core/src/experimental/components/Tree/Tree.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
 
-export interface TreeProps extends React.HTMLProps<HTMLDivElement> {
+export interface TreeProps extends React.HTMLProps<HTMLTableElement> {
   /** Additional classes added to the Tree. */
   className?: string;
 }
 
 export const Tree: React.FunctionComponent<TreeProps> = ({ className = '', ...props }) => (
-  <div {...props}>
-    <h1>TODO: implement a tree here</h1>
-  </div>
+  <table className="pf-c-table pf-m-grid-lg" role="grid" {...props}>
+    <thead>
+      <tr>
+        <td />
+        <th scope="col">Column 1</th>
+        <th scope="col">Column 2</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td className="pf-c-table__check">
+          <input type="checkbox" name="item-1" aria-labelledby="item-1-label" />
+        </td>
+        <th data-label="Item 1">
+          <div id="item-1-label">Item 1</div>
+        </th>
+        <td data-label="Info">Info 1</td>
+      </tr>
+      <tr>
+        <td className="pf-c-table__check">
+          <input type="checkbox" name="item-2" aria-labelledby="item-2-label" />
+        </td>
+        <th data-label="Item 2">
+          <div id="item-2-label">Item 2</div>
+        </th>
+        <td data-label="Info">Info 2</td>
+      </tr>
+    </tbody>
+  </table>
 );
